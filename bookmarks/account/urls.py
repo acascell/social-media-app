10cd3f36299322feaa1265d2633c3e618c4d3999
Antonio Cascella <infocascella@gmail.com>
2024-04-08 17:41:24 +0200

    feat: updated django reset view/urls/templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -12,5 +12,13 @@ urlpatterns = [
 
     # change password urls
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password-change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # reset password urls
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uibd64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_completessss'),
+
 ]
