1b9761d3e1a27bb77c3a918b014992310ba306fc
Antonio Cascella <infocascella@gmail.com>
2024-04-08 12:34:05 +0200

    feat: defined built-in django auth system
    - created login/logout view
    - defined landing page
    - generated html templates with conditional branches

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login")
+    # custom login view
+    #path("login/", views.user_login, name="login")
+    # django login/logout view
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('', views.dashboard, name='dashboard')
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': dashboard})
 
+
+# custom log-in view
 def user_login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
