b0963353f8de43bc5dc220f7eac0e70b2bd455e5
Antonio Cascella <infocascella@gmail.com>
2024-04-08 10:25:33 +0200

    feat: defined user auth
    - using middleware session auth offered by django, created login form, base html, updated urls

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class LoginForm(forms.Form):
     """This form is used to authenticate the user against the database"""
 
-    username = forms.CharField(password=forms.CharField(widget=forms.PasswordInput))
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
