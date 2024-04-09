bae84c8250a650c01e7ecf162f49c89dc2671609
Antonio Cascella <infocascella@gmail.com>
2024-04-09 13:08:59 +0200

    feat: defined email auth system
    - updated template, forms, views
    - defined backend system in settings.py

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from . import forms
 from .models import Profile
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
@@ -58,10 +59,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-        #     messages.success(request, 'Profile updated '\
-        #                               'successfully')
-        # else:
-        #     messages.error(request, 'Error updating your profile')
+            messages.success(request, "Profile updated " "successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = forms.UserEditForm(instance=request.user)
         profile_form = forms.ProfileEditForm(instance=request.user.profile)
