c9906d5d41d7e50aed396fdbeb6bf3f2e08b6066
Antonio Cascella <infocascella@gmail.com>
2024-04-09 12:42:54 +0200

    feat: updated forms
    - allowed user to edit profile and user info
    - updated urls pattern and views
    - updated landing page creating new href links to edit pages

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
@@ -22,3 +23,23 @@ class UserRegistrationForm(forms.ModelForm):
         cd = self.cleaned_data
         if cd["password"] != cd["password2"]:
             raise ValidationError("Password dont match.")
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+    # def clean_email(self):
+    #     data = self.cleaned_data['email']
+    #     qs = User.objects.exclude(id=self.instance.id)\
+    #                      .filter(email=data)
+    #     if qs.exists():
+    #         raise forms.ValidationError('Email already in use.')
+    #     return data
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["date_of_birth", "photo"]
