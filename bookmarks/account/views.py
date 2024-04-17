1304f2c20469140d5040024f46b127833350204c
acascell <infocascella@gmail.com>
2024-04-17 15:04:11 +0200

    feat: defined follow system
    - each user can follow each other, the template will render the follower and display it on the page

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import login, authenticate
+from django.views.decorators.http import require_POST
+
 from . import forms
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -87,3 +89,21 @@ def user_detail(request, username):
     return render(
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
