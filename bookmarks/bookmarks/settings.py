ea06fa0f205e8aaebadd685ef8edc4565ebae013
acascell <infocascella@gmail.com>
2024-04-18 11:40:05 +0200

    feat: enabled django debug toolbar

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     "images.apps.ImagesConfig",
     "easy_thumbnails",
     "actions.apps.ActionsConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -171,3 +173,7 @@ if DEBUG:
 ABSOLUTE_URL_OVERRIDES = {
     "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
 }
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
