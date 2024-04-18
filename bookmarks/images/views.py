9e0603b9d827e749c601175e5970d0fd22519cff
acascell <infocascella@gmail.com>
2024-04-18 12:15:23 +0200

    feat: updated images view and template
    - used redis to store total views

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -6,11 +6,16 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
 from django.views.decorators.http import require_POST
-
 from actions.utils import create_action
-
+import redis
+from django.conf import settings
 
 # Create your views here.
+
+# connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -39,8 +44,14 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f"image:{image.id}:views")
+    # increment image ranking by 1
+    r.zincrby("image_ranking", 1, image.id)
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
